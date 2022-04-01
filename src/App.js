a2aad142611e4abc6908986c86f9c1de726587ff
kaslerushi <kaslerushikesh19@gmail.com>
2022-04-01 13:56:41 +0530

    change 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import {BrowserRouter as Router,Route} from "react-router-dom"
 import Home from './components/Home'
 import Login from './components/user/Login'
 import Register from './components/user/Register'
@@ -8,11 +8,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
           <Route exact path='/' component={Home}/>
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register}/>
-        </Routes>
       </Router>
     </div>
   );
